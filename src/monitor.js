b751824fd6e49dcf424f56baa78329de3bb76ad3
Pablo Alcayaga <1160066+alcayaga@users.noreply.github.com>
2026-01-26 22:23:15 -0300

    refactor: remove monitor.js and rename monitor.test.js

--- src/monitor.js
+++
@@ -1,8 +0,0 @@
-/**
- * This module exports the Discord client instance.
- * @module monitor
- */
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-module.exports = { client };
